0c3b4731f59995ec6b8bc3a191cf9bf552012d26
alvin tan <alvintan95@gmail.com>
2024-03-23 10:23:07 +0800

    use tmpl instead of html

--- trainer/controller.go
+++ trainer/controller.go
@@ -33,7 +33,7 @@ func (c *Controller) Home(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error getting random question: %s", err)
 	}
-	tmpl, err := template.ParseFS(c.tmpl, "base.tmpl")
+	tmpl, err := template.ParseFS(c.html, "base.tmpl")
 	if err != nil {
 		log.Printf("Error parsing template: %s", err)
 	}
@@ -48,7 +48,7 @@ func (c *Controller) NewQuestion(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error getting random question: %s", err)
 	}
-	tmpl, err := template.ParseFS(c.tmpl, "game.tmpl")
+	tmpl, err := template.ParseFS(c.html, "game.tmpl")
 	if err != nil {
 		log.Printf("Error parsing template: %s", err)
 	}
@@ -78,7 +78,7 @@ func (c *Controller) Result(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Error getting choices: %s", err)
 	}
 
-	tmpl, err := template.ParseFS(c.tmpl, "result.tmpl")
+	tmpl, err := template.ParseFS(c.html, "result.tmpl")
 	if err != nil {
 		log.Printf("Error parsing template: %s", err)
 	}
