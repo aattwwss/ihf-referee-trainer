4da3b5d0ee69defbb534fb372bb5e15a2c147c1f
alvin tan <alvintan95@gmail.com>
2024-06-25 01:58:18 +0800

    add validation for input

--- public/static/quizConfig.js
+++ public/static/quizConfig.js
@@ -1,7 +1,7 @@
 const multiSelectWithoutCtrl = (elemSelector) => {
     let options = [].slice.call(document.querySelectorAll(`${elemSelector} option`));
-    options.forEach(function(element) {
-        element.addEventListener("mousedown", function(e) {
+    options.forEach(function (element) {
+        element.addEventListener("mousedown", function (e) {
             e.preventDefault();
             element.parentElement.focus();
             this.selected = !this.selected;
@@ -10,6 +10,15 @@ const multiSelectWithoutCtrl = (elemSelector) => {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     multiSelectWithoutCtrl('#rules');
 });
+
+document.getElementById('num-questions').addEventListener('input', function (e) {
+    e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+    const numQuestions = parseInt(e.target.value);
+    const maxNumQuestions = parseInt(e.target.max);
+    if (numQuestions > maxNumQuestions) {
+        e.target.value = maxNumQuestions;
+    }
+})
