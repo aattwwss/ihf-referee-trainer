b13a68edf870ea13d5ec3171750df43ec422db41
alvin tan <alvintan95@gmail.com>
2024-06-25 02:10:30 +0800

    add number validation to duration

--- public/static/quizConfig.js
+++ public/static/quizConfig.js
@@ -14,11 +14,22 @@ document.addEventListener('DOMContentLoaded', function () {
     multiSelectWithoutCtrl('#rules');
 });
 
-document.getElementById('num-questions').addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/[^0-9.]/g, '');
-    const numQuestions = parseInt(e.target.value);
-    const maxNumQuestions = parseInt(e.target.max);
-    if (numQuestions > maxNumQuestions) {
-        e.target.value = maxNumQuestions;
-    }
-})
+// document.getElementById('num-questions').addEventListener('input', function (e) {
+//     e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+//     const numQuestions = parseInt(e.target.value);
+//     const maxNumQuestions = parseInt(e.target.max);
+//     if (numQuestions > maxNumQuestions) {
+//         e.target.value = maxNumQuestions;
+//     }
+// })
+
+const textInputs = Array.from(document.getElementsByClassName('number-input'));
+textInputs.forEach(el => el.addEventListener('input', function (e) {
+        e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+        const numQuestions = parseInt(e.target.value);
+        const maxNumQuestions = parseInt(e.target.max);
+        if (!isNaN(maxNumQuestions) && numQuestions > maxNumQuestions) {
+            e.target.value = maxNumQuestions;
+        }
+    })
+)
