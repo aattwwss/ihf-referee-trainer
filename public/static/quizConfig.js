e1eebacbd7c29a071b4d3ec8a0c9c88f47aa46aa
alvin <alvin@debian.local>
2024-07-14 20:02:32 +0800

    add validatin for 0 num questions

--- public/static/quizConfig.js
+++ public/static/quizConfig.js
@@ -1,7 +1,7 @@
 const multiSelectWithoutCtrl = (elemSelector) => {
     let options = [].slice.call(document.querySelectorAll(`${elemSelector} option`));
-    options.forEach(function (element) {
-        element.addEventListener("mousedown", function (e) {
+    options.forEach(function(element) {
+        element.addEventListener("mousedown", function(e) {
             e.preventDefault();
             element.parentElement.focus();
             this.selected = !this.selected;
@@ -10,29 +10,32 @@ const multiSelectWithoutCtrl = (elemSelector) => {
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     multiSelectWithoutCtrl('#rules-filter');
 });
 
-const textInputs = Array.from(document.getElementsByClassName('number-input'));
-textInputs.forEach(el => el.addEventListener('input', function (e) {
-        e.target.value = e.target.value.replace(/[^0-9.]/g, '');
-        const numQuestions = parseInt(e.target.value);
-        const maxNumQuestions = parseInt(e.target.max);
-        if (!isNaN(maxNumQuestions) && numQuestions > maxNumQuestions) {
-            e.target.value = maxNumQuestions;
-        }
-    })
+const numberInputs = Array.from(document.getElementsByClassName('number-input'));
+numberInputs.forEach(el => el.addEventListener('input', function(e) {
+    e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+    const numQuestions = parseInt(e.target.value);
+    const maxNumQuestions = parseInt(e.target.max);
+    if (numQuestions === 0) {
+        e.target.value = null;
+    }
+    if (!isNaN(maxNumQuestions) && numQuestions > maxNumQuestions) {
+        e.target.value = maxNumQuestions;
+    }
+})
 )
 
-document.getElementById('rules-filter-select-all').addEventListener('click', function () {
+document.getElementById('rules-filter-select-all').addEventListener('click', function() {
     const select = document.getElementById('rules-filter');
     for (let i = 0; i < select.options.length; i++) {
         select.options[i].selected = true;
     }
 });
 
-document.getElementById('rules-filter-clear-all').addEventListener('click', function () {
+document.getElementById('rules-filter-clear-all').addEventListener('click', function() {
     const select = document.getElementById('rules-filter');
     for (let i = 0; i < select.options.length; i++) {
         select.options[i].selected = false;
